24ad00661d912bb284ed4c5c2ced13b1b3433d2e
aryanpamwani123 <aryanpamwani123@gmail.com>
2024-10-12 14:29:47 +0000

    Changes Fake

--- src/components/items/Spinner/Spinner.jsx
+++ src/components/items/Spinner/Spinner.jsx
@@ -5,6 +5,8 @@ const Spinner = () => {
     <div className='my-20 bg-[var(--grey-006)] opacity-55 border rounded-2xl w-16 h-16 flex justify-center items-center m-auto]'> 
     <span className="loader "></span>
     </div>
+
+    
   )
 }
 
