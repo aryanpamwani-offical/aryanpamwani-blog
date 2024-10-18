ee4a26f64b6609dbe2d0aa68ab855711f1ab01d3
aryanpamwani-offical <aryanpamwani@outlook.com>
2024-10-18 16:09:38 +0530

    Added Close Button on the Nav Resp

--- src/components/items/Navbar/NavResp.jsx
+++ src/components/items/Navbar/NavResp.jsx
@@ -6,7 +6,7 @@ import { navItems } from './NavData';
 import Image from 'next/image';
 import { useSelector } from 'react-redux';
 import { useTheme } from '@/components/Features/reducers/useTheme';
-
+import { MdOutlineClose } from "react-icons/md";
 const NavResp = () => {
   const [status, setStatus] = useState(false);
   const [lightTheme] =useTheme();
@@ -19,7 +19,10 @@ const NavResp = () => {
         <Link href={"/"}><Image src="https://res.cloudinary.com/dttek3gqg/image/upload/v1724921045/navlogo_a1hivv.webp" width={80} height={80} className='w-20 pl-6 ml-10' alt='logo' priority={true} /></Link>
         </div>
         <div className="flex justify-end w-2/3 p-5" onClick={() => setStatus(!status)}>
+{!status? 
           <Image src={"https://res.cloudinary.com/dttek3gqg/image/upload/v1724922497/menu_crwwxl.png"} width={56} height={56} className={`${lightTheme ? 'w-14' : 'w-14 icon-dark'} my-auto pl-6 ml-5`} priority={true} alt='menu' />
+
+:<MdOutlineClose  className={`${lightTheme ? 'w-14 text-[var(--grey-001)]' : 'w-14 icon-dark'} my-auto pl-6 ml-5 text-4xl text-[var(--grey-001)]`} priority={true} alt='menu'/>}
         </div>
 
         <ul className={`flex flex-col space-grotesk transition-all ease-in-out ${status ? 'opacity-100 translate-y-0 duration-1000' : 'opacity-0 -translate-y-10 duration-500'} content-center items-center justify-center m-auto w-full`}>
