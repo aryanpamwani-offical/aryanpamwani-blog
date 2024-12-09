49fbc819ae0d2a49544954322e04e9c69526a334
aryanpamwani-offical <113785853+aryanpamwani-offical@users.noreply.github.com>
2024-12-09 09:33:00 +0000

    Bug Fixes:Sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,18 +1,35 @@
 export default function sitemap() {
-  return [
-    {
-      url: 'https://blog.aryanpamwani.in/',
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      
-      priority: 1,
-    },
-    {
-      url: 'https://blog.aryanpamwani.in/blog',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    
-  ]
-}
+    return [
+      {
+        url: 'https://aryanpamwani.in/',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        
+        priority: 1,
+      },
+      {
+        url: 'https://aryanpamwani.in/about',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://aryanpamwani.in/skills',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://aryanpamwani.in/projects',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.3,
+      },
+      {
+        url: 'https://aryanpamwani.in/contact',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.3,
+      },
+    ]
+  }
