c8b3e6af0fe1e45dba141767e09ef2feddd182b5
aryanpamwani-offical <113785853+aryanpamwani-offical@users.noreply.github.com>
2024-12-09 09:30:31 +0000

    Bug Fixes:Sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,38 +1,18 @@
-import { fetchPost } from "@/lib/apiCalls";
-
- const sitemap = async () => {
-  try {
-    // const posts = await fetchPost();
-
-    // if (!posts ) {
-    //   throw new Error("No posts found");
-    // }
-
-    // const postUrls = posts.map((post) => ({
-    //   url: `https://blog.aryanpamwani.in/blog/${post.slug}`,
-    //   changeFrequency: 'weekly',
-    //   priority: 1,
-    //   lastModified: new Date(post.Date).toISOString(),
-    // }));
-
-    return [
-      {
-        url: `https://blog.aryanpamwani.in/`,
-        changeFrequency: 'weekly',
-        priority: 1,
-        lastModified: new Date().toISOString(),
-      },
-      {
-        url: `https://blog.aryanpamwani.in/blog`,
-        changeFrequency: 'weekly',
-        priority: 1,
-        lastModified: new Date().toISOString(),
-      },
-      // ...postUrls,
-    ];
-  } catch (error) {
-    console.error("Failed to generate sitemap:", error);
-    return [];
-  }
-};
-export default sitemap
+export default function sitemap() {
+  return [
+    {
+      url: 'https://blog.aryanpamwani.in/',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      
+      priority: 1,
+    },
+    {
+      url: 'https://blog.aryanpamwani.in/blog',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    
+  ]
+}
