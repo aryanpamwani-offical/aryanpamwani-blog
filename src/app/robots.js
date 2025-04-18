d9ec5205dceb3a9a538eee2c7b3640ad560d3d1c
aryanpamwani-offical <aryanpamwani@outlook.com>
2025-04-18 17:56:30 +0530

    feat: Add sitemap generation and robots.txt configuration

---
+++ src/app/robots.js
@@ -0,0 +1,14 @@
+
+
+export default function robots()  {
+
+ return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/studio',
+    },
+    sitemap: 'https://blog.aryanpamwani.in/sitemap.xml',
+  }
+}
+
