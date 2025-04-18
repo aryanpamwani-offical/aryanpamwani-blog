27c0c66e62a5d44fb5427c3e7aaee4eecd7e7651
aryanpamwani-offical <aryanpamwani@outlook.com>
2025-04-18 17:01:43 +0530

    fix: Update sitemap reference in robots.txt and adjust sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,10 +4,16 @@ const { fetchPostsForSitemap } = require('./src/lib/sitemapApi');
 module.exports = {
     siteUrl: 'https://blog.aryanpamwani.in', // Use your actual domain
     generateRobotsTxt: true,
-    exclude: ['/manifest.webmanifest', '/some-other-non-page'], // Ensure nothing important is excluded
+    exclude: ['/manifest.webmanifest', '/some-other-non-page'],
+    robotsTxtOptions: {
+        additionalSitemaps: [
+          'https://blog.aryanpamwani.in/sitemap-0.xml' // Update with the correct sitemap
+        ],
+    },
+     // Ensure nothing important is excluded
     additionalPaths: async (config) => {
         const blogPosts = await fetchPostsForSitemap();
-        console.log('Blog posts for sitemap:', blogPosts); // Debugging line to check fetched posts
+        // console.log('Blog posts for sitemap:', blogPosts); // Debugging line to check fetched posts
         return [
           { loc: '/', changefreq: 'daily', priority: 1.0 },
           { loc: '/blog', changefreq: 'daily', priority: 0.9 },
